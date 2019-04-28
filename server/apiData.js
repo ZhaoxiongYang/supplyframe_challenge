af0f2191053fda8e6185672785097b2b248fb5e2
ZhaoxiongYang <xiong@ruis-mbp.attlocal.net>
2019-04-28 13:07:50 -0700

    set api key check

--- server/apiData.js
+++ server/apiData.js
@@ -8,12 +8,11 @@ apiData.apiKey = '?api_key=' + apiData.userKey;
 apiData.apiUrl = 'https://api.hackaday.io/v1';
 apiData.apiAuthUrl = 'https://api.hackaday.io/v1/me' + apiData.apiKey;
 apiData.oAuthRedirect = 'https://hackaday.io/authorize?client_id=' + apiData.clientId + '&response_type=code';
-apiData.createTokenUrl = function (code) {
-    return ('https://auth.hackaday.io/access_token?' +
-        'client_id=' + this.clientId +
-        '&client_secret=' + this.clientSecret +
-        '&code=' + code +
-        '&grant_type=authorization_code');
-};
+
+if (!apiData.userKey || !apiData.clientId || !apiData.clientSecret) {
+    console.log('Please correct your Hackaday Client ID, Client Secret and API Key ! ');
+    console.log('Ending node process.');
+    process.exit();
+}
 
 global.apiData = apiData;
